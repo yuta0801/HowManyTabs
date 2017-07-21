78febb92d2dc63fce5915610998a3dbfd1ab6121
yuta <yuta20030801@icloud.com>
2017-07-21 15:57:56 +0900

    i18n対応

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.browserAction.onClicked.addListener(tab => {
 	var tablen = 0, winlen = 0;
-
 	chrome.windows.getAll({}, windows => {
 		winlen=windows.length;
 		chrome.tabs.query({}, tabs => {
 			tablen=tabs.length;
 			chrome.tabs.create({
-				url: 'https://twitter.com/intent/tweet?hashtags=howmanytabs&text='+encodeURIComponent(`現在、${winlen}個のウィンドウで、${tablen}個のタブを開いています。`)
+				url: 'https://twitter.com/intent/tweet?hashtags=howmanytabs&text=' +
+				encodeURIComponent(chrome.i18n.getMessage("extTweetMsg").replace('winlen', winlen).replace('tablen', tablen))
 			});
 		});
 	});
